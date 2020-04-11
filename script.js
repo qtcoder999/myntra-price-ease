63f466e5fffbf2e5d0509a247d7b8b2ba11d50e0
qtcoder999 <30284500+qtcoder999@users.noreply.github.com>
2020-04-12 04:20:44 +0530

    Added eventListener on paginations.
    Refactored code and added eventListener on pagination numbers

--- script.js
+++ script.js
@@ -1,9 +1,54 @@
-let x = document.querySelectorAll('.product-discountedPrice')
+// ==UserScript==
+// @name         Myntra Price Ease
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       Paras Anand
+// @match        https://www.myntra.com/men-casual-shirts?p=2&sort=popularity
+// @grant        none
+// ==/UserScript==
 
-let priceYouWant = 501;
+(function() {
+  "use strict";
 
-for(i in x){
-	const disountedPriceStr = x[i].innerText && x[i].innerText !== 'undefined' ? x[i].innerText.replace('Rs. ','') : null;
-	const discountedPriceNumber = disountedPriceStr && disountedPriceStr !== 'undefined' ? parseInt(disountedPriceStr) : null;
-	discountedPriceNumber < priceYouWant && discountedPriceNumber !== null ? console.log(discountedPriceNumber) : null
-}
+  const main = () => {
+    console.clear();
+
+    let arr = [];
+
+    let discountedHTMLElements = document.querySelectorAll(
+      ".product-discountedPrice"
+    );
+
+    let priceYouWant = 500;
+
+    for (let discountedElement of discountedHTMLElements) {
+      const discoutedPrice = discountedElement.innerText;
+
+      const discountedPriceNumber = parseInt(
+        discoutedPrice && discoutedPrice !== "undefined"
+          ? discoutedPrice.replace("Rs. ", "")
+          : null
+      );
+
+      if (
+        discountedPriceNumber < priceYouWant &&
+        discountedPriceNumber !== null
+      ) {
+        //   console.log(discountedPriceNumber);
+        arr.push(discountedPriceNumber);
+      }
+    }
+
+    arr.sort();
+    console.log(arr);
+  };
+
+  const cbox = document.querySelectorAll(".pagination-number");
+
+  for (let i = 0; i < cbox.length; i++) {
+    cbox[i].addEventListener("click", main);
+  }
+
+  setTimeout(main, 2000);
+})();
